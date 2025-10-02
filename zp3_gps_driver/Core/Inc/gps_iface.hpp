1a10186e3641376975ec63e31e5fccf379425f18
Rosnaky <ronakjpatelr22@gmail.com>
2025-10-02 16:18:01 -0400

    add gps driver

---
+++ zp3_gps_driver/Core/Inc/gps_iface.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "gps_datatypes.hpp"
+
+//#ifdef __cplusplus
+#include <cstdint>
+//#endif
+
+
+class IGPS {
+protected:
+	IGPS() = default;
+
+public:
+	virtual ~IGPS() = default;
+
+	virtual GpsData_t readData() = 0;
+};
