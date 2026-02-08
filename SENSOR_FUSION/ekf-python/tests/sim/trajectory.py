c5e2da61bbc665a09097a4629a976dab180d4969
Aditya Bang <adityabang15@gmail.com>
2026-02-08 17:23:30 -0500

    move constant motion trajectory class to iframe

--- SENSOR_FUSION/ekf-python/tests/sim/trajectory.py
+++ SENSOR_FUSION/ekf-python/tests/sim/trajectory.py
@@ -5,27 +5,35 @@ from pyekf.utils import to_col_vector
 from pyekf.quaternions import quaternion_exponential, multiply_quaternions
 
 class ConstantMotionTrajectory:
-    def __init__(self, displacement_initial, velocity_initial, quaternion_initial, accel_body, omega_body):
+    def __init__(
+        self, 
+        displacement_initial_iframe: NDArray[np.float64], 
+        velocity_initial_iframe: NDArray[np.float64], 
+        quaternion_initial_iframe: NDArray[np.float64], 
+        accel_constant_iframe: NDArray[np.float64], 
+        angular_vel_constant_iframe: NDArray[np.float64]
+    ):
+        self.displacement_initial_iframe: NDArray[np.float64] = to_col_vector(displacement_initial_iframe, 3)
+        self.velocity_initial_iframe: NDArray[np.float64] = to_col_vector(velocity_initial_iframe, 3)
+        self.quaternion_initial_iframe: NDArray[np.float64] = to_col_vector(quaternion_initial_iframe, 4)
+        self.accel_constant_iframe: NDArray[np.float64] = to_col_vector(accel_constant_iframe, 3)
+        self.angular_vel_constant_iframe: NDArray[np.float64] = to_col_vector(angular_vel_constant_iframe, 3)
+
+    def get_state(self, t: float) -> tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]]:
         """
-        displacement_initial, velocity_initial: (3, 1) column vectors
-        quaternion_initial: (4, 1) column vector [w, x, y, z]^T
-        accel_body: (3, 1) linear acceleration
-        omega_body: (3, 1) angular velocity
+        Returns the state of the trajectory at time t in the inertial frame. The state includes:
+        - Displacement (3x1 vector)
+        - Velocity (3x1 vector)
+        - Quaternion (4x1 vector)
+        - Angular velocity (3x1 vector)
         """
-        self.displacement_initial = to_col_vector(displacement_initial, 3)
-        self.velocity_initial = to_col_vector(velocity_initial, 3)
-        self.quaternion_initial = to_col_vector(quaternion_initial, 4)
-        self.accel_body = to_col_vector(accel_body, 3)
-        self.angular_vel_body = to_col_vector(omega_body, 3)
+        # Integration of orientation in inertial frame
+        angle_axis_iframe = self.angular_vel_constant_iframe * t
+        q_delta = quaternion_exponential(angle_axis_iframe)
+        quaternion_iframe = multiply_quaternions(q_delta, self.quaternion_initial_iframe)
 
-    def get_state(self, t: float) -> tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]]:
-        # 1. Orientation
-        angle_axis = self.angular_vel_body * t
-        q_delta = quaternion_exponential(angle_axis)
-        q_t = multiply_quaternions(self.quaternion_initial, q_delta)
+        # Integration of constant acceleration in inertial frame
+        velocity_iframe = self.velocity_initial_iframe + self.accel_constant_iframe * t
+        displacement_iframe = self.displacement_initial_iframe + self.velocity_initial_iframe * t + 0.5 * self.accel_constant_iframe * (t**2)
 
-        # 2. Displacement + Velocity
-        v_t = self.velocity_initial + self.accel_body * t
-        p_t = self.displacement_initial + self.velocity_initial * t + 0.5 * self.accel_body * (t**2)
-
-        return p_t, v_t, q_t
+        return displacement_iframe, velocity_iframe, quaternion_iframe, self.angular_vel_constant_iframe
