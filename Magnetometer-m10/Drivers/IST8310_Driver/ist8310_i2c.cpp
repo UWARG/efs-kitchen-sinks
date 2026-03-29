5c49f450c46c8828031879757744e3b98450724e
Aust1n-moon <jeonghyeon076@gmail.com>
2026-03-29 18:01:41 -0400

    header file for driver

---
+++ Magnetometer-m10/Drivers/IST8310_Driver/ist8310_i2c.cpp
@@ -0,0 +1,10 @@
+/*
+ * ist8310_i2c.cpp
+ *
+ *  Created on: Mar 1, 2026
+ *      Author: jeong
+ */
+#include "ist8310_i2c.hpp"
+
+
+
---
+++ Magnetometer-m10/Drivers/IST8310_Driver/ist8310_i2c.hpp
@@ -0,0 +1,32 @@
+/*
+ * ist8310_i2c.hpp
+ *
+ *  Created on: Mar 1, 2026
+ *      Author: jeong
+ */
+
+#ifndef IST8310_DRIVER_IST8310_I2C_HPP_
+#define IST8310_DRIVER_IST8310_I2C_HPP_
+
+#include "main.h"
+#include <stdint.h>
+#include <stddef.h>
+#include "stm32l5xx_it.h"
+
+class ist8310_i2c
+{
+private:
+	/* data */
+public:
+	ist8310_i2c(/* args */);
+	~ist8310_i2c();
+};
+
+ist8310_i2c::ist8310_i2c(/* args */)
+{
+}
+
+ist8310_i2c::~ist8310_i2c()
+{
+}
+
